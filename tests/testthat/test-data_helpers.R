fe400273cc83eca7c9d19ac651d579b12fe441cd
Duy Nguyen <duyn@sccwrp.org>
2024-11-19 14:43:38 -0800

    started to implement unit testing for this app

---
+++ tests/testthat/test-data_helpers.R
@@ -0,0 +1,15 @@
+
+# make a folder in testthat called test_data
+# put the test data in there .csv
+# make a
+# usethis::use_test(
+#   ctr shft T to run all tests
+
+
+#load
+# test_that("multiplication works", {
+#   #expect_equal(get_concentrationplot_data(p1,p2), "test" )
+#   expect_equal(data.frame(name= c('duy','nick'), school=c(2,1)),
+#                data.frame(name= c('duy','nick'), school=c('2','1')))
+#
+# })
